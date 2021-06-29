3f322ad8677720df54271599267b8ce0e62a6dc7
Francis <terrero0611@gmail.com>
2021-06-28 22:13:57 -0400

    Icon and page titles

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -17,62 +17,98 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      title: 'Login'
+    }
   },
   {
     path: '/delivery-register',
     name: 'DeliveryRegister',
-    component: DeliveryRegister
+    component: DeliveryRegister,
+    meta: {
+      title: 'Registrar delivery'
+    }
   },
   {
     path: '/local-register',
     name: 'LocalRegister',
-    component: LocalRegister
+    component: LocalRegister,
+    meta: {
+      title: 'Registrar local'
+    }
   },
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Pedidos Los Frailes'
+    }
   },
   {
     path: '/shop/:id',
     name: 'Shop',
-    component: Shop
+    component: Shop,
+    meta: {
+      title: 'Tienda'
+    }
   },
   {
     path: '/shop/:id/checkout',
     name: 'Checkout',
-    component: Checkout
+    component: Checkout,
+    meta: {
+      title: 'Checkout'
+    }
   },
   {
     path: '/local/:id',
     name: 'Local',
-    component: Local
+    component: Local,
+    meta: {
+      title: 'Local Dashboard'
+    }
   },
   {
     path: '/local-info',
     name: 'LocalInfo',
-    component: LocalInfo
+    component: LocalInfo,
+    meta: {
+      title: 'Información del local'
+    }
   },
   {
     path: '/local/:id/order/:orderId',
     name: 'Order',
-    component: Order
+    component: Order,
+    meta: {
+      title: 'Información de orden'
+    }
   },
   {
     path: '/delivery/:id',
     name: 'Delivery',
-    component: Delivery
+    component: Delivery,
+    meta: {
+      title: 'Delivery Dashboard'
+    }
   },
   {
     path: '/delivery-info',
     name: 'DeliveryInfo',
-    component: DeliveryInfo
+    component: DeliveryInfo,
+    meta: {
+      title: 'Información del delivery'
+    }
   },
   {
     path: '/admin/:id',
     name: 'Admin',
-    component: Admin
+    component: Admin,
+    meta: {
+      title: 'Admin Dashboard'
+    }
   },
   {
     path: '/:pathMatch(.*)',
@@ -100,6 +136,44 @@ router.beforeEach((to, from, next) => {
   if (authRequired && !loggedIn) {
     return next('/login')
   }
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
+  // `/nested`'s will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title
+  } else if (previousNearestWithMeta) {
+    document.title = previousNearestWithMeta.meta.title
+  }
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el))
+
+  // Skip rendering meta tags if there are none.
+  if (!nearestWithMeta) return next()
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta')
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key])
+    })
+
+    // We use this to track which meta tags we create so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '')
+
+    return tag
+  })
+  // Add the meta tags to the document head.
+    .forEach(tag => document.head.appendChild(tag))
 
   next()
 })
