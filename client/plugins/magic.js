9b19f822067afbf8688f7910f13f0877f76f956b
Francis Terrero <fjoante@gmail.com>
2022-05-13 20:16:32 -0400

    test

--- client/plugins/magic.js
+++ client/plugins/magic.js
@@ -5,4 +5,5 @@ const createMagic = (key) => {
   return typeof window !== 'undefined' && new Magic(key)
 }
 
+console.log(process.env.MAGIC_PUBLISHABLE_KEY)
 export const magic = createMagic(process.env.MAGIC_PUBLISHABLE_KEY)
