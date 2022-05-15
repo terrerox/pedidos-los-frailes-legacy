67c139c3f5a6447157e895b2c1184b2824be1373
Francis Terrero <fjoante@gmail.com>
2022-05-15 17:02:06 -0400

    delete console.log

--- client/plugins/magic.js
+++ client/plugins/magic.js
@@ -5,7 +5,6 @@ export default function ({ $config: { magicApiSecret, baseURL } }, inject) {
     return typeof window !== 'undefined' && new Magic(key)
   }
 
-  console.log({ baseURL, magicApiSecret })
   const magic = createMagic(magicApiSecret)
   inject('magic', magic)
 }
