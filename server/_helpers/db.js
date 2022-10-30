7fa51930ff43073ff1b3b3ce105642a1aa6c93ac
Francis <terrero0611@gmail.com>
2022-10-30 16:59:07 -0400

    default credentials deleted

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -9,11 +9,11 @@ initialize();
 
 async function initialize() {
   const connectionData = {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'terrerox',
-    password: process.env.DB_PASS || 'joanwinner0611',
-    database: process.env.DB_NAME || 'pedidoslosfrailes',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   }
   const {
     host,
