9dc6efee0030080fe9b0a6cb9bad7181bb085b46
Francis <terrero0611@gmail.com>
2022-11-06 00:01:11 -0400

    host

--- server/index.js
+++ server/index.js
@@ -25,4 +25,4 @@ app.use(errorHandler);
 // start server
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5000;
-app.listen(port, host, () => console.log('Server listening on port ' + port));
+app.listen(port, host, () => console.log('Server listening on port ' + port + ' and host '+ host));
